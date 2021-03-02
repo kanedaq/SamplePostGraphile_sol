a575c33e5d1e01f272aea0542fb550619f17e808
kaneda <kaneda@makoto.nifty.jp>
2021-03-02 18:07:19 +0900

    (1)データソースをweather.jsonからPostGraphileに変更します

--- SamplePostGraphile/Program.cs
+++ SamplePostGraphile/Program.cs
@@ -1,3 +1,4 @@
+using SamplePostGraphile.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace SamplePostGraphile
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<WeatherForecastService>();
 
             await builder.Build().RunAsync();
         }
